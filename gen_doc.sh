55564fbab6e3112188f9f20da28f88d3e360bf1d
Ofir Gal <ofir@gal.software>
2022-12-05 11:18:54 +0200

    added basic doc

---
+++ gen_doc.sh
@@ -0,0 +1,1 @@
+lemmy-help --prefix-func --prefix-alias --prefix-class --prefix-type lua/title-nvim/init.lua > doc/title-nvim.txt
--- lua/title-nvim/init.lua
+++ lua/title-nvim/init.lua
@@ -20,17 +20,29 @@ local config = common.default_config
 ---
 ----- Or setup with custom parameters
 ---require('title-nvim').setup {
---- TODO: [doc]
+---    -- Preview window settings
+---    preview = {
+---        highlight = 'Comment', -- Highlight group of the preview
+---        border = 'rounded', -- Border of the preview window
+---    },
+---    -- Default title settings, `require('title-nvim').title({title_opts})`
+---    default_title = {
+---        text = 'Title', -- Text
+---        len = 40, -- Length
+---        filler_sequence = '-', -- Filler sequence
+---        lines_amount = 3, -- Amount of lines of the title
+---    }
 ---}
 ---@usage ]]
 M.setup = function(user_config)
-	user_config = user_config or {}
-	config = vim.tbl_deep_extend('keep', user_config, common.default_config)
+    user_config = user_config or {}
+    config = vim.tbl_deep_extend('keep', user_config, common.default_config)
 end
 
 api.nvim_create_user_command('Title', function()
-	window.new_title(config.default_title)
+    window.new_title(config.default_title)
 end, {})
 
+M.title = window.new_title
 
 return M
